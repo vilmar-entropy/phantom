04241a98aae85f8289ed73a2e75bf2a0c4de4f47
vilmar-entropy <vilmar.entropy@gmail.com>
2017-05-07 01:32:06 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,18 @@
 //$('#stress').hide();
 //alert("hello");
 
-$("#solution1_1").hide();
-function solution1_1() {
-  $("#solution1_1").fadeToggle("slow");
+
+$("#thermo_solution1_1").hide();
+function thermo_solution1_1() {
+  $("#thermo_solution1_1").fadeToggle("slow");
+  if($("#thermo_answer1_1").is(':visible')) {
+	  $("#thermo_answer1_1").hide();
+  }
+}
+$("#thermo_answer1_1").hide();
+function thermo_answer1_1() {
+  $("#thermo_answer1_1").fadeToggle("slow");
+  if($("#thermo_solution1_1").is(':visible')) {
+	  $("#thermo_solution1_1").hide();
+  }
 }
